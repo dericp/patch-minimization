5b036f4686d6bdfa133b6f43c5cb39cb3dd9e072
Deric Hua Pang <dericp@cs.washington.edu>
2015-08-26 16:06:58 -0700

    Updated the run_all_Lang script to work with the new package.

--- run_all_Lang.sh
+++ run_all_Lang.sh
@@ -1,7 +1,7 @@
 #!/opt/local/bin/bash
-javac -d bin -sourcepath src -cp bin:lib/diff-utils.jar src/edu/washington/bugisolation/BugMinimizer.java
+javac -d bin -sourcepath src -cp bin src/edu/washington/bugisolation/BugMinimizer.java
 for i in {1..65}
 do
-    printf "%s\n" "Lang" "$i" "true" | java -ea -cp bin:lib/diff-utils.jar edu.washington.bugisolation.BugMinimizer
-    printf "%s\n" "Lang" "$i" "false" | java -ea -cp bin:lib/diff-utils.jar edu.washington.bugisolation.BugMinimizer
+    printf "%s\n" "Lang" "$i" "true" | java -ea -cp bin edu.washington.bugisolation.BugMinimizer
+    printf "%s\n" "Lang" "$i" "false" | java -ea -cp bin edu.washington.bugisolation.BugMinimizer
 done
