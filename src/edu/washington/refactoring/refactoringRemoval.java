1663acb5fbd6f42a130e2c01d6eb2ccc765a0348
Deric Pang <dericp@cs.washington.edu>
2016-01-05 17:39:04 -0800

    Added skeleton for refactoring functions class.

---
+++ src/edu/washington/refactoring/refactoringRemoval.java
@@ -0,0 +1,23 @@
+package edu.washington.refactoring;
+
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
+/**
+ * This class contains the functions that automatically refactor code.
+ * @author dpang
+ *
+ */
+public class refactoringRemoval {
+    
+    public static void removeWhiteSpace() {
+        throw new NotImplementedException();
+    }
+    
+    public static void removeCommentsDocumntation() {
+        throw new NotImplementedException();
+    }
+    
+    public static void removeIrrelevantAnnotations() {
+        throw new NotImplementedException();
+    }
+}
