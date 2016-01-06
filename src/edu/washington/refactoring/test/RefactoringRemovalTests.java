e332808f87e028d07d50caa74d6720f82ab47008
Deric Pang <dericp@cs.washington.edu>
2016-01-06 11:16:44 -0800

    Added unit testing skeleton.

---
+++ src/edu/washington/refactoring/test/RefactoringRemovalTests.java
@@ -0,0 +1,14 @@
+package edu.washington.refactoring.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RefactoringRemovalTests {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
