05c2607bdbb6dbc0e9806d0deb026d0e36821993
Deric Pang <dericp@cs.washington.edu>
2016-01-05 17:38:42 -0800

    Updated main method documentation.

--- src/edu/washington/refactoring/main/AutoRefactoringMain.java
+++ src/edu/washington/refactoring/main/AutoRefactoringMain.java
@@ -1,5 +1,10 @@
 package edu.washington.refactoring.main;
 
+/**
+ * This class runs the automatic refactoring program.
+ * @author dpang
+ *
+ */
 public class AutoRefactoringMain {
 
     public static void main(String[] args) {
