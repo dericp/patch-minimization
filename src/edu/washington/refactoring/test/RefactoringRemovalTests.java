931d201b05e51b03955c2029c3c2674d2c9071d9
Deric Pang <dericp@cs.washington.edu>
2016-01-06 19:27:11 -0800

    Updated project for use with IntelliJ.

--- src/edu/washington/refactoring/test/RefactoringRemovalTests.java
+++ src/edu/washington/refactoring/test/RefactoringRemovalTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class RefactoringRemovalTests {
-
+    // just testing if this syncs
     @Test
     public void test() {
         fail("Not yet implemented");
